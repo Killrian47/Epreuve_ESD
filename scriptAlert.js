6018de526c931c9089b9097cf8736230ea1bec48
Your Name <killian.portier@yahoo.fr>
2022-05-19 22:15:33 +0200

    épreuve 2ème jour

---
+++ scriptAlert.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('click');
+function clicked() {
+    alert("Vos réponses ont été enregistrées")
+}
+function clicked1() {
+    alert("Vos coordonnées ont été transmises")
+}
